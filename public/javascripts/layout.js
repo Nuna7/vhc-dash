3f6af6c6bdff26b98ddf87bca46b458c891ed277
Aarush Kumbhakern <aarushbeleren11@gmail.com>
2024-12-18 18:03:01 +0530

    HIJACK ZOOM CONTROLS

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -1,5 +1,33 @@
 // capture elements
+const content = document.getElementById("Content");
 const navLinks = document.querySelectorAll("#Navigation .link");
+
+// initialize page font size
+const savedSize = localStorage.getItem("fontSize");
+if (savedSize) content.style.fontSize = savedSize;
+
+// initialize settings counters
+updateSettingsDisplay();
+
+// increment/decrement base font size (12 <= x <= 18)
+function zoom(mode) {
+	const fontSize = Math.max(Math.min(parseFloat(window.getComputedStyle(content).fontSize) + (mode == "in" ? 1 : -1), 18), 12);
+	content.style.fontSize = `${fontSize}px`;
+	localStorage.setItem("fontSize", fontSize);
+	
+	updateSettingsDisplay();
+}
+
+// update settings counters
+function updateSettingsDisplay() {
+	document.getElementById("fontSize").innerHTML = window.getComputedStyle(content).fontSize;
+}
+
+// site zoom hotkeys
+hotkeys("ctrl+=, ctrl+-", function (event, handler) {
+	event.preventDefault();
+	zoom(handler.key == "ctrl+=" ? "in" : "out");
+});
 
 // response navigation hotkeys
 hotkeys("ctrl+0, ctrl+1, ctrl+2, ctrl+3, ctrl+4, ctrl+5, ctrl+6, ctrl+7, ctrl+8, ctrl+9", function (event, handler) {
