7d5669c286920e79cb9be84e6a658733e01a01db
Aarush Kumbhakern <aarushbeleren11@gmail.com>
2024-12-20 20:36:02 +0530

    VISUALLY MARK OCCUPIED RANKS
    Also replace forEach iteration with for ... of and fix rank radio event listener

--- public/javascripts/ranker.js
+++ public/javascripts/ranker.js
@@ -2,40 +2,46 @@
 // capture elements
 const responses = document.querySelectorAll(".Response");
 const rankers = document.querySelectorAll(".Response .ranker");
-const rank_radios = document.querySelectorAll(".Response .ranker input[type='radio']");
-const status_radios = document.querySelectorAll(".Response .status input[type='radio']");
+const rankRadios = document.querySelectorAll(".Response .ranker input[type='radio']");
+const statusRadios = document.querySelectorAll(".Response .status input[type='radio']");
 const review = document.getElementById("Review");
 const form = document.getElementById("Ballot");
 
 // initialize page
-updateRadio();
+markTaken();
+passFailSplit();
 updateRankData();
 updateReviewBox();
 
 // ranker state change event listener
-Array.from(rank_radios).forEach(element => {
+Array.from(rankRadios).forEach(element => {
 	element.addEventListener("change", function(e) {		
 		// erase duplicate ranker states
-		Array.from(rank_radios).filter(radio => radio !== this).forEach(radio => {
-			if (radio.value == this.value) { radio.checked = false; }
-		});
+		if (this.checked) {
+			for (const radio of Array.from(rankRadios).filter(radio => radio !== this)) {
+				if (radio.value == this.value) radio.checked = false;
+			}
+		}
 		
+		markTaken();
 		updateRankData();
 		updateReviewBox();
 	}, false);
 });
 
 // status state change event listener
-Array.from(status_radios).forEach(element => {
-	element.addEventListener("change", function(e) { updateRadio(); }, false);
+Array.from(statusRadios).forEach(element => {
+	element.addEventListener("change", function(e) { 
+		passFailSplit(); 
+	}, false);
 });
 
 // update response element rank data
 function updateRankData() {
-	Array.from(rankers).forEach(ranker => {
+	for (const ranker of rankers) {
 		const checkedRadio = ranker.querySelector("input[type='radio']:checked");
 		ranker.parentElement.querySelector(".rankDisplay").innerText = checkedRadio ? "#" + String(checkedRadio.value) : "?";
-	})
+	}
 }
 
 // update review box state
@@ -44,7 +50,7 @@ function updateReviewBox() {
 	var ranking = Array(rankers.length).fill("<span class='tGray'>?</span>");
 
 	// iterate over rankers and...
-	Array.from(rankers).forEach(ranker => {
+	for (const ranker of rankers) {
 		const checkedRadio = ranker.querySelector("input[type='radio']:checked");
 		if (checkedRadio) { 
 			// ...count number of ranked responses
@@ -53,7 +59,7 @@ function updateReviewBox() {
 			// ...update rank array
 			ranking[checkedRadio.value - 1] = ranker.parentNode.querySelector(".modelName").innerText; 
 		}
-	});
+	};
 
 	// update rank preview display and disable/enable submission button
 	review.innerHTML = `${ranking[0]} > ${ranking[1]} > ${ranking[2]} > ${ranking[3]}`;
@@ -61,26 +67,40 @@ function updateReviewBox() {
 }
 
 // update radio values
-function updateRadio() {
-	Array.from(rank_radios).forEach(radio => { radio.disabled = false; });
+function passFailSplit() {
+	for (const radio of rankRadios) radio.disabled = false;
 
 	var pass_count = 0;
 
-	Array.from(responses).forEach(response => {
+	// count no. of passing responses
+	for (const response of responses) {
 		const status = response.querySelector(".status input[type='radio']:checked").value;
 		if (status == "pass") pass_count++;
-	});
+	}
 
-	Array.from(responses).forEach(response => {
+	// disable appropriate rank range for each response
+	for (const response of responses) {
 		const status = response.querySelector(".status input[type='radio']:checked").value;
 		
-		Array.from(response.querySelectorAll(".ranker input[type='radio']")).forEach(radio => {
+		for (const radio of response.querySelectorAll(".ranker input[type='radio']")) {
 			if ((status == "fail" && radio.value <= pass_count) || (status == "pass" && radio.value > pass_count)) { 
 				radio.disabled = true; 
 				radio.checked = false; 
+				radio.dispatchEvent(new Event("change"));
 			}
-		});
-	});
+		}
+	}
+}
+
+// visually mark occupied ranks
+function markTaken() {
+	const selectedRadios = document.querySelectorAll(".Response .ranker input[type='radio']:checked");
+	const selectedValues = Array.from(selectedRadios).map(radio => radio.value);
+
+	for (const radio of rankRadios) {
+		if (selectedValues.includes(radio.value)) radio.classList.add("sTaken");
+		else radio.classList.remove("sTaken");
+	}
 }
 
 // response navigation hotkeys
@@ -123,7 +143,7 @@ hotkeys("p,f", function (event, handler) {
 
 	if (e.classList.contains("Response")) {
 		e.querySelector(`#radio-${e.id.replace("response-", "")}-${handler.key}`).checked = true;
-		updateRadio();
+		passFailSplit();
 	}
 });
 
