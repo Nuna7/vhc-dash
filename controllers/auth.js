c93cd601f6be45ebc666dc558d24a0620482e62e
Aarush Kumbhakern <aarushbeleren11@gmail.com>
2025-02-11 18:55:00 +0530

    IMPROVE AUTH ERROR HANDLING

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,36 @@
+const { body, check, validationResult } = require("express-validator");
+const mongoose = require("mongoose");
+
+const User = require("../models/User");
+
 exports.login_get = (req, res, next) => {
 	if (req.query.next) { req.session.returnTo = req.query.next }
 	res.render("auth/login", { title: "Log In" });
 }
+
+exports.login_validate_post = [
+	body("username").custom((value) => {
+		return User.findOne({ username: value }).then(user => {
+			if (!user) { return Promise.reject("User does not exist"); }
+			if (!user.approved) { return Promise.reject("User not approved"); }
+			
+			return true;
+		});
+		
+	}),
+	
+	(req, res, next) => {
+		const errors = validationResult(req);
+		console.log(errors);
+		
+		if (!errors.isEmpty()) { 
+			req.session.errors = errors.array();
+			return res.redirect("/login");
+		} 
+
+		next();
+	}
+]
 
 exports.login_success_post = (req, res, next) => {
 	const returnTo = req.session.returnTo;
@@ -10,10 +39,12 @@ exports.login_success_post = (req, res, next) => {
 }
 
 exports.login_error_post = (err, req, res, next) => {
-	res.render("auth/login", { 
-		title: "Log In",
-		error: err 
-	});
+	req.session.errors = [{ 
+		path: "password", 
+		msg: "Incorrect passkey" 
+	}];
+
+	return res.redirect("/login");
 }
 
 exports.logout = (req, res, next) => {
--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,7 @@ const passport = require('passport');
 const authController = require("../controllers/auth");
 
 router.get("/login", authController.login_get);
-router.post("/login", passport.authenticate("local", { failWithError: true }), authController.login_success_post, authController.login_error_post);
+router.post("/login", authController.login_validate_post, passport.authenticate("local", { failWithError: true }), authController.login_success_post, authController.login_error_post);
 
 router.post("/logout", authController.logout);
 
