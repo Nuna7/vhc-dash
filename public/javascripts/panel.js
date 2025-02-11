18a941ac7e2285eb6f58e734310e0527293f00cf
Aarush Kumbhakern <aarushbeleren11@gmail.com>
2025-02-11 18:47:27 +0530

    CONDITIONALLY DISABLE INFO FORM SUBMIT BUTTON

--- public/javascripts/panel.js
+++ public/javascripts/panel.js
@@ -1,5 +1,25 @@
+const infoForm = document.getElementById("InfoForm");
+const infoFormSubmit = infoForm.querySelector("button[type='submit']");
+const infoFormElements = infoForm.querySelectorAll("input");
 const ORCiDButton = document.getElementById("ORCiDButton");
 const ORCiDSection = document.getElementById("ORCiDSection");
+
+// disable submit button if no form elements are changed
+infoFormSubmit.disabled = true;
+
+for (const element of infoFormElements) {
+	if (element.value != element.getAttribute("value")) {
+		infoFormSubmit.disabled = false;
+		break;
+	}
+}	
+
+// enable submit button if any form element is changed
+for (const element of infoFormElements) {
+	element.addEventListener("input", function() {
+		infoFormSubmit.disabled = (element.value == element.getAttribute("value"));
+	});
+}
 
 // show ORCiD field if there are form errors
 if (document.querySelector(".FormErrors li[data-field='orcid']")) {
