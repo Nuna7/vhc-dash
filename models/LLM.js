b8771b5b9062ae5a6a3fe76a36e4758d0809038c
Aarush Kumbhakern <aarushbeleren11@gmail.com>
2025-02-15 19:57:09 +0530

    MAKE VERSION AND PARAMS OPTIONAL

--- models/LLM.js
+++ models/LLM.js
@@ -4,13 +4,18 @@ const Schema = mongoose.Schema;
 
 const LLMSchema = new Schema({
 	name: { type: String, required: true },
-	version: { type: Schema.Types.Decimal128, required: true },
-	parameters: { type: Schema.Types.Decimal128, required: true },
+	version: { type: Schema.Types.Decimal128 },
+	parameters: { type: Schema.Types.Decimal128 },
 }, { timestamps: { createdAt: "created", updatedAt: "edited" } });
 
 // concatenate model data into display information
 LLMSchema.virtual("displayName").get(function() {
-	return `${this.name}-${this.parameters}b${this.version ? "-v" + String(this.version) : ""}`;
+	var display = this.name;
+
+	if (this.parameters) display += `-${this.parameters}b`;
+	if (this.version) display += `-v${this.version}`;
+
+	return display;
 });
 
 module.exports = mongoose.model("LLM", LLMSchema);
