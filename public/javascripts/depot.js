b201fe258011220e35e6d6c2691d0bad4f5cc317
Aarush Kumbhakern <aarushbeleren11@gmail.com>
2025-02-16 01:15:56 +0530

    KEYBOARD SHORTCUTS FOR USER DEPOT

--- public/javascripts/depot.js
+++ public/javascripts/depot.js
@@ -22,3 +22,38 @@ for (const radio of statusRadios) {
 		depotFormSubmit.disabled = Array.from(statusRadios).filter((r) => r.checked && r.value == "defer" ).length == roleInputs.length;
 	});
 }
+
+// table navigation hotkeys
+hotkeys("up,down,esc", function (event, handler) {
+	const e = document.activeElement;
+	
+	if (e.classList.contains("RegistrationRequest")) {
+		switch (handler.key) {
+			case "up":
+			case "down":
+				const edgeChild = document.querySelector(`.RegistrationRequest:${handler.key == "up" ? "first" : "last"}-of-type`);
+
+				if (e !== edgeChild) handler.key == "up" ? e.previousSibling.focus() : e.nextSibling.focus();
+				else edgeChild.focus();
+	
+				break;
+	
+			case "esc": 
+				e.blur(); 
+				break;
+		}
+	}
+});
+
+// request status hotkeys
+hotkeys("`", function (event, handler) {
+	const e = document.activeElement;
+
+	if (e.classList.contains("RegistrationRequest")) {
+		const radios = [...e.querySelectorAll(".statusField input[type='radio']")];
+		const nextIndex = (radios.findIndex(r => r.checked) + 1) % radios.length;
+		
+		radios[nextIndex].checked = true;
+		radios[nextIndex].dispatchEvent(new Event("change"));
+	}
+});
