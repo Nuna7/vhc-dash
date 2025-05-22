8fb24156330ce4225af96f2d1d8c2052d0be9bb3
Aarush Kumbhakern <aarushbeleren11@gmail.com>
2025-05-22 17:14:26 +0530

    IMPROVE COMMENTS

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,6 @@
 exports.sessionAuthCheck = function (roles) {
 	return function (req, res, next) {
+		// if logged in, verify permissions
 		if (req.isAuthenticated()) {
 			if (roles && !roles.some(role => req.user.roles.includes(role))) {
 				const error = new Error("You don't have permission to access this page");
@@ -7,7 +8,7 @@ exports.sessionAuthCheck = function (roles) {
 				return next(error);
 			} 
 			
-			else return next();
+			return next();
 		} 
 		
 		// retrieve and reset passthrough flash if exists
@@ -16,11 +17,13 @@ exports.sessionAuthCheck = function (roles) {
 			delete req.session.PTFlash;
 		}
 
+		// redirect to login (and save current url for easy return) if not logged in
 		req.session.returnTo = req.originalUrl || req.url;
 		res.redirect("/login");
 	}
 }
 
+// make auth data available to templates
 exports.sessionAuthData = function (req, res, next) {
 	if (req.isAuthenticated()) { res.locals.user = req.user; } 
 	return next();
