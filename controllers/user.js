b3756f4ed78a3fa2e289da08e5f864f287f3fccd
Aarush Kumbhakern <aarushbeleren11@gmail.com>
2025-05-23 12:51:58 +0530

    :art: wrap too-long lines

--- controllers/user.js
+++ controllers/user.js
@@ -14,7 +14,12 @@ export function panel(req, res, next) {
 // edit user details -----------------------------------------------------------
 export const edit_user = [
 	body("email", "Invalid email ID").trim().isEmail().custom(async (value, { req }) => {
-		try { return (await User.exists({ _id: { $ne: req.user._id }, email: value })) ? Promise.reject("User with this email ID exists") : true; }
+		try {
+			return (await User.exists({ _id: { $ne: req.user._id }, email: value }))
+				? Promise.reject("User with that email ID exists")
+				: true;
+		}
+
 		catch (err) { next(err); }
 	}),
 
