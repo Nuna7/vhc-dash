f63ddbca8001a8304da7e4742804f73123b8976a
Aarush Kumbhakern <aarushbeleren11@gmail.com>
2025-05-23 11:03:15 +0530

    :lock: implement rate limiting

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,48 @@
 import { Router } from "express";
+import rateLimit from "express-rate-limit";
 import passport from "passport";
 
+import { flashAndRedirect } from "../utils/flash.js";
+
 import authController from "../controllers/auth.js";
+
+// RATE LIMITERS ===============================================================
+
+const LOGIN_RATE_MIN = 5
+const REG_RATE_MIN = 10
+const ATTEMPTS = 5
+
+// login -----------------------------------------------------------------------
+const loginAuthLimiter = rateLimit({
+	windowMs: LOGIN_RATE_MIN * 60 * 1000,
+	max: ATTEMPTS,
+
+	handler: (req, res) => flashAndRedirect(req, res, "message", {
+		type: "warn",
+		msg: `Too many auth. attempts from this IP. Please wait ${LOGIN_RATE_MIN} minutes.`
+	}, "/login")
+});
+
+// registration ----------------------------------------------------------------
+const registrationAuthLimiter = rateLimit({
+	windowMs: REG_RATE_MIN * 60 * 1000,
+	max: ATTEMPTS,
+
+	handler: (req, res) => flashAndRedirect(req, res, "message", {
+		type: "warn",
+		msg: `Too many registration requests from this IP. Please wait ${REG_RATE_MIN} minutes.`
+	}, "/register")
+});
 
 // ROUTING =====================================================================
 
 const router = Router();
 
 router.get("/login", authController.login_get);
-router.post("/login", authController.login_validate_post, passport.authenticate("local", { failWithError: true }), authController.login_success_post, authController.login_error_post);
+router.post("/login", loginAuthLimiter, authController.login_validate_post, passport.authenticate("local", { failWithError: true }), authController.login_success_post, authController.login_error_post);
 
 router.get("/register", authController.register_get);
-router.post("/register", authController.register_post);
+router.post("/register", registrationAuthLimiter, authController.register_post);
 
 router.post("/logout", authController.logout);
 
--- routes/ranker.js
+++ routes/ranker.js
@@ -1,15 +1,36 @@
 import { Router } from "express";
+import rateLimit from "express-rate-limit";
+
+import { flashAndRedirect } from "../utils/flash.js";
 
 import { sessionAuthCheck } from "../middleware/auth.js";
 
 import rankerController from "../controllers/ranker.js";
+
+// RATE LIMITERS ===============================================================
+
+const RATE_MIN = 1
+const ATTEMPTS = 5
+
+const ballotLimiter = rateLimit({ // -------------------------------------------
+	windowMs: RATE_MIN * 60 * 1000,
+	max: ATTEMPTS,
+
+	// rate‐limit by user rather than IP
+	keyGenerator: req => req.user._id.toString(),
+
+	handler: (req, res) => flashAndRedirect(req, res, "message", {
+		type: "warn",
+		msg: "User response rate too high. Please wait 5 minutes."
+	}, "/ranker")
+});
 
 // ROUTING =====================================================================
 
 const router = Router();
 
 router.get("/", sessionAuthCheck(), rankerController.ranker);
-router.post("/", sessionAuthCheck(), rankerController.post_ballot);
+router.post("/", sessionAuthCheck(), ballotLimiter, rankerController.post_ballot);
 
 // DEFAULT EXPORT ==============================================================
 
